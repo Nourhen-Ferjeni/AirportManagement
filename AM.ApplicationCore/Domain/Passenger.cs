cf13c6a15493a69d6f572089f99a9764089f0de3
Nourhen-Ferjeni <nourhen.ferjeni26@gmail.com>
2024-11-02 11:05:50 +0100

    base de donnée

--- AM.ApplicationCore/Domain/Passenger.cs
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,24 @@ namespace AM.ApplicationCore.Domain
 {
     public class Passenger
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
+        [Key]
+        [StringLength(7)]
         public string PassportNumber { get; set; }
+
+        [MinLength(3, ErrorMessage = "Min 3 caractères")]
+        [MaxLength(25, ErrorMessage = "Max 25 caractères")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
+        [RegularExpression("^[0,9]{8}$")]
         public string TelNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
         
         // Relation * avec Flights
